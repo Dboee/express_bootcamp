2126a8ddc2293bbad078edfc5d0ea882129aa2ab
Dboee <davidboee@gmail.com>
2020-01-05 12:41:51 +0100

    hello express world

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Out express app will go here");
+// SETUP
+var express = require("express");
+var app = express();
+
+
+// ROUTES
+app.get("/", function(req, res){
+    res.send("Hi there!");
+});
+
+
+// LISTENER
+app.listen(3000, function() { 
+  console.log('Server listening on port 3000'); 
+});
