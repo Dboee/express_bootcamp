b6210b9f44afa06f8cef48ff4eb3d78fad0adb5a
Dboee <davidboee@gmail.com>
2020-01-05 12:51:22 +0100

    Added the dog get

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ app.get("/", function(req, res){
     res.send("Hi there!");
 });
 
+app.get("/bye", function(req,res){
+    res.send("Byetho");
+});
+
+app.get("/dog", function(req,res){
+    res.send("MOEW");
+    console.log("SOMEONE DID A DOG REQUEST")
+});
+
 
 // LISTENER
 app.listen(3000, function() { 
